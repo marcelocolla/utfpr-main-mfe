7c834aed8f8c4bdca52ab54b5f710bbb8b8be3c2
Marcelo Colla <mcolla.dev@gmail.com>
2022-05-21 19:38:37 -0300

    adiciona mensagem de campo obrigatório ao fazer login

--- src/components/FormSignIn/FormSignIn.schema.ts
+++ src/components/FormSignIn/FormSignIn.schema.ts
@@ -1,6 +1,6 @@
 import { object, string } from 'yup'
 
 export const SignInSchema = object().shape({
-  email: string().required('Required'),
-  password: string().required('Required'),
+  email: string().required('Insira seu e-mail de acesso!'),
+  password: string().required('Insira sua senha de acesso!'),
 })
